cb58381e3a83c3a17a9fe26ce6e2e3c1d6a09e8f
cracker <crackerwork@126.com>
2023-11-17 15:01:23 +0800

    增加仪表板UI

--- src/Simple.Admin.Web.Host/wwwroot/component/pear/pear.js
+++ src/Simple.Admin.Web.Host/wwwroot/component/pear/pear.js
@@ -14,6 +14,7 @@ layui.config({
 	menu: "menu",		         // 数据菜单组件
 	frame: "frame", 	         // 内容页面组件
 	tab: "tab",			         // 多选项卡组件
+	echarts: "echarts",          // 数据图表组件
 	echartsTheme: "echartsTheme",// 数据图表主题
 	encrypt: "encrypt",		     // 数据加密组件
 	select: "select",	         // 下拉多选组件
